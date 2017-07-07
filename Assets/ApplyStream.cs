ee6c3df39702c24c09de237cc949fc57e63009af
Seyoung <seyoung.arts.park@protonmail.com>
2017-07-07 11:14:22 +0300

    Capture Color buffer from secondary camera

---
+++ Assets/ApplyStream.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ApplyStream : MonoBehaviour {
+
+    private Camera cam;
+    public RenderTexture tex;
+    public GameObject display;
+    // Use this for initialization
+    //private Shader shader;
+    [Range(0f, 3f)]
+    public float depthLevel = 0.5f;
+
+
+    //private Material _material;
+    //private Material material
+    //{
+    //    get
+    //    {
+    //        if (_material == null)
+    //        {
+    //            _material = new Material(shader);
+    //            _material.hideFlags = HideFlags.HideAndDontSave;
+    //        }
+    //        return _material;
+    //    }
+    //}
+
+
+
+    void Start () {
+        cam = GetComponent<Camera>();
+        cam.targetTexture = tex;
+        
+
+        //if (!SystemInfo.supportsImageEffects)
+        //{
+        //    print("System doesn't support image effects");
+        //    enabled = false;
+        //    return;
+        //}
+        //if (shader == null || !shader.isSupported)
+        //{
+        //    enabled = false;
+        //    print("Shader " + shader.name + " is not supported");
+        //    return;
+        //}
+
+        // turn on depth rendering for the camera so that the shader can access it via _CameraDepthTexture
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //material.SetFloat("_DepthLevel", depthLevel);
+
+        RenderTexture.active = tex;
+        cam.Render();
+        //Graphics.Blit(tex, tex, material);
+
+        Texture2D t = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
+        // Read pixels from screen into the saved texture data.
+        t.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+        // Actually apply all previous SetPixel and SetPixels changes.
+        t.Apply();    // Unnecessary?
+
+        display.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = t;
+
+        RenderTexture.active = null;
+    }
+}
---
+++ Assets/SetQuadColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetQuadColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
