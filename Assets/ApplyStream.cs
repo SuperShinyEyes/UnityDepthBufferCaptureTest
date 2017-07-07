141d10e66ab1361e75410b3e92dcfdde9ad78311
Seyoung <seyoung.arts.park@protonmail.com>
2017-07-07 13:54:08 +0300

    Capture depth buffer from main camera

--- Assets/ApplyStream.cs
+++ Assets/ApplyStream.cs
@@ -9,24 +9,24 @@ public class ApplyStream : MonoBehaviour {
     public RenderTexture tex;
     public GameObject display;
     // Use this for initialization
-    //private Shader shader;
+    public Shader shader;
     [Range(0f, 3f)]
     public float depthLevel = 0.5f;
 
 
-    //private Material _material;
-    //private Material material
-    //{
-    //    get
-    //    {
-    //        if (_material == null)
-    //        {
-    //            _material = new Material(shader);
-    //            _material.hideFlags = HideFlags.HideAndDontSave;
-    //        }
-    //        return _material;
-    //    }
-    //}
+    private Material _material;
+    private Material material
+    {
+        get
+        {
+            if (_material == null)
+            {
+                _material = new Material(shader);
+                _material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _material;
+        }
+    }
 
 
 
@@ -35,18 +35,18 @@ public class ApplyStream : MonoBehaviour {
         cam.targetTexture = tex;
         
 
-        //if (!SystemInfo.supportsImageEffects)
-        //{
-        //    print("System doesn't support image effects");
-        //    enabled = false;
-        //    return;
-        //}
-        //if (shader == null || !shader.isSupported)
-        //{
-        //    enabled = false;
-        //    print("Shader " + shader.name + " is not supported");
-        //    return;
-        //}
+        if (!SystemInfo.supportsImageEffects)
+        {
+            print("System doesn't support image effects");
+            enabled = false;
+            return;
+        }
+        if (shader == null || !shader.isSupported)
+        {
+            enabled = false;
+            print("Shader " + shader.name + " is not supported");
+            return;
+        }
 
         // turn on depth rendering for the camera so that the shader can access it via _CameraDepthTexture
         cam.depthTextureMode = DepthTextureMode.Depth;
@@ -55,18 +55,18 @@ public class ApplyStream : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        //material.SetFloat("_DepthLevel", depthLevel);
+        material.SetFloat("_DepthLevel", depthLevel);
 
         RenderTexture.active = tex;
-        cam.Render();
-        //Graphics.Blit(tex, tex, material);
+        //cam.Render();
+        Graphics.Blit(tex, tex, material);
 
         Texture2D t = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
         // Read pixels from screen into the saved texture data.
         t.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
 
         // Actually apply all previous SetPixel and SetPixels changes.
-        t.Apply();    // Unnecessary?
+        t.Apply();    // Unnecessary? YES Necessary!!!
 
         display.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = t;
 
---
+++ Assets/DisplayColorStream.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayColorStream : MonoBehaviour {
+    private Camera cam;
+    public RenderTexture tex;
+    public GameObject display;
+    // Use this for initialization
+    
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.targetTexture = tex;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderTexture.active = tex;
+        //cam.Render();
+        Texture2D t = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
+        // Read pixels from screen into the saved texture data.
+        t.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+        // Actually apply all previous SetPixel and SetPixels changes.
+        t.Apply();    // Unnecessary? YES Necessary!!!
+
+        display.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = t;
+
+        RenderTexture.active = null;
+    }
+}
